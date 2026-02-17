040c788ef3dd3ed75b80e3e8b5056685a113e6e9
magiccart <team.magiccart@gmail.com>
2026-02-17 14:06:56 +0700

    Update Category.php

--- Helper/Category.php
+++ Helper/Category.php
@@ -10,51 +10,8 @@ namespace Magepow\Categories\Helper;
  *
  * @SuppressWarnings(PHPMD.LongVariable)
  */
-class Category extends \Magento\Framework\App\Helper\AbstractHelper
+class Category extends \Magento\Catalog\Helper\Category
 {
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $storeManager;
-
-    /**
-     * @var \Magento\Catalog\Model\CategoryFactory
-     */
-    protected $categoryInstance;
-
-    /**
-     * @var \Magento\Catalog\Api\CategoryRepositoryInterface
-     */
-    protected $categoryRepository;
-
-    /**
-     * @param \Magento\Framework\App\Helper\Context $context
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Catalog\Model\CategoryFactory $categoryFactory
-     * @param \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository
-     */
-    public function __construct(
-        \Magento\Framework\App\Helper\Context $context,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
-        \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository,
-    ) {
-
-        $this->storeManager = $storeManager;
-        $this->categoryRepository = $categoryRepository;
-        $this->categoryInstance = $categoryFactory->create();
-        parent::__construct($context);
-    }
-
-    /**
-     * GetStoreId
-     *
-     * @return int
-     */
-    public function getStoreId()
-    {
-        return $this->storeManager->getStore()->getId();
-    }
 
     public function getCategory($categoryId)
     {
@@ -63,7 +20,7 @@ class Category extends \Magento\Framework\App\Helper\AbstractHelper
   
     public function getCategories($id)
     {
-        return $this->categoryInstance->getCategories($id);
+        return $this->_categoryFactory->create()->getCategories($id);
     }
     
 }
